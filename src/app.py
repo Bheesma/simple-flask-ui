52f26e409c15d137f9442a132dbda280acdeea71
prakash <prakash@prakash.com>
2022-09-02 18:19:43 +1000

    Trim the fat

--- src/app.py
+++ src/app.py
@@ -1,7 +1,12 @@
-from flask import render_template
-from core import app
+
+from flask import Flask,render_template
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     greeting="Hello World - Simple Flask UI !!!"
     return render_template('index.html', greet=greeting)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
